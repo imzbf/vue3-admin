93e50ef6f96b2586dce8349ad605b532ff95eda8
zbf <zbfcqtl@163.com>
2021-01-27 10:45:58 +0800

    refactor: 分离NavBar

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -10,53 +10,7 @@
     </div>
     <div class="layout">
       <header class="layout-header">
-        <div class="layout-bar">
-          <ul class="layout-bar-left">
-            <li @click="adjustMenu" class="cper"><i :class="data.menuTouchClass" /></li>
-            <li class="breadcrumb-help">
-              <el-breadcrumb separator="/">
-                <el-breadcrumb-item
-                  v-for="(breadcrumb, index) of breadcrumbs"
-                  :key="breadcrumb + index"
-                  >{{ breadcrumb }}</el-breadcrumb-item
-                >
-              </el-breadcrumb>
-            </li>
-          </ul>
-          <ul class="layout-bar-right">
-            <el-popover placement="top-end" trigger="click">
-              <template #reference>
-                <li>
-                  <el-badge :value="12" class="item">
-                    <i class="el-icon-bell" />
-                  </el-badge>
-                </li>
-              </template>
-              <el-tabs value="first">
-                <el-tab-pane label="通知" name="first">通知</el-tab-pane>
-                <el-tab-pane label="消息" name="second">消息</el-tab-pane>
-                <el-tab-pane label="邮件" name="third">邮件</el-tab-pane>
-              </el-tabs>
-            </el-popover>
-            <li><i class="el-icon-table-lamp" /></li>
-            <li @click="fullScreen">
-              <i :class="data.isFullscreen ? 'el-icon-aim' : 'el-icon-full-screen'" />
-            </li>
-            <li><i class="el-icon-setting" /></li>
-            <el-dropdown>
-              <li><el-avatar size="small" :src="HeadImg" /></li>
-              <template #dropdown>
-                <el-dropdown-menu>
-                  <el-dropdown-item icon="el-icon-user">个人中心</el-dropdown-item>
-                  <el-dropdown-item icon="el-icon-setting">个人设置</el-dropdown-item>
-                  <el-dropdown-item icon="el-icon-switch-button" divided
-                    >退出登录</el-dropdown-item
-                  >
-                </el-dropdown-menu>
-              </template>
-            </el-dropdown>
-          </ul>
-        </div>
+        <NavBar />
       </header>
       <main class="layout-main">
         <router-view />
@@ -66,19 +20,13 @@
 </template>
 
 <script lang="ts">
-import { onMounted, computed, defineComponent, reactive, watch } from 'vue';
+import { onMounted, computed, defineComponent } from 'vue';
 import { useStore } from 'vuex';
 import { key } from '@/store';
-import screenfull from 'screenfull';
-import { ElMessage } from 'element-plus';
+
 import Menu from './Menu/index.vue';
-
+import NavBar from './NavBar/index.vue';
 import LogoImg from '@/assets/logo.png';
-import HeadImg from '@/assets/head02.gif';
-
-export interface SetUpReturn {
-  collapse: boolean;
-}
 
 export default defineComponent({
   setup() {
@@ -88,21 +36,6 @@ export default defineComponent({
     onMounted(() => {
       store.commit('menu/routeChanged');
     });
-
-    const data = reactive({
-      logoShow: store.state.setting.aside === 'open',
-      menuTouchClass: 'el-icon-s-fold',
-      // 全屏状态
-      isFullscreen: false
-    });
-
-    // 隐藏显示菜单图标样式，watch写法
-    watch(
-      () => store.state.setting.aside === 'open',
-      (state) => {
-        data.menuTouchClass = state ? 'el-icon-s-fold' : 'el-icon-s-unfold';
-      }
-    );
 
     // 侧边栏展开状态
     const asideOpen = computed(() => {
@@ -116,46 +49,13 @@ export default defineComponent({
         : 'layout-aside layout-aside-close';
     });
 
-    const breadcrumbs = computed(() => store.state.setting.breadcrumbs);
-
-    const adjustMenu = () => {
-      store.commit('setting/asideState', {
-        aside: store.state.setting.aside === 'open' ? 'close' : 'open'
-      });
-    };
-
-    if (screenfull.isEnabled) {
-      screenfull.on('change', () => {
-        data.isFullscreen = !data.isFullscreen;
-      });
-    }
-
-    const fullScreen = () => {
-      if (screenfull.isEnabled) {
-        if (screenfull.isFullscreen) {
-          screenfull.exit();
-        } else screenfull.request();
-      } else {
-        ElMessage({
-          showClose: true,
-          message: '浏览器不支持全屏',
-          type: 'error'
-        });
-      }
-    };
-
     return {
-      adjustMenu,
       asideOpen,
-      data,
-      breadcrumbs,
       layoutAsideClass,
-      fullScreen,
-      LogoImg,
-      HeadImg
+      LogoImg
     };
   },
-  components: { Menu }
+  components: { Menu, NavBar }
 });
 </script>
 
@@ -235,49 +135,6 @@ export default defineComponent({
 
   .layout {
     flex: 1;
-
-    .layout-header {
-      .layout-bar {
-        display: flex;
-        justify-content: space-between;
-        box-shadow: 0 1px 4px rgb(220, 223, 230);
-
-        .layout-bar-left,
-        .layout-bar-right {
-          position: relative;
-          display: flex;
-
-          li {
-            height: 50px;
-            padding: 0 10px;
-            display: inline-flex;
-            align-items: center;
-          }
-        }
-
-        .layout-bar-left {
-          @media screen and (max-width: 750px) {
-            .breadcrumb-help {
-              display: none;
-            }
-          }
-        }
-
-        .layout-bar-right {
-          padding-right: 10px;
-
-          li {
-            cursor: pointer;
-            padding: 0 16px;
-            transition: all 0.3s;
-
-            &:hover {
-              background-color: rgb(244, 244, 245);
-            }
-          }
-        }
-      }
-    }
 
     .layout-main {
       padding: 10px;
