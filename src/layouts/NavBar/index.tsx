ddd4fa500fd89f6be2d799a6d8c833162084562f
zhoubangfu <zbfcqtl@163.com>
2021-02-03 16:28:27 +0800

    refactor: layout nav-bar适配antd-vue

--- src/layouts/NavBar/index.tsx
+++ src/layouts/NavBar/index.tsx
@@ -1,10 +1,20 @@
-import { defineComponent, computed, reactive, watch } from 'vue';
+import { defineComponent, computed, reactive } from 'vue';
 import { useStore } from 'vuex';
 import { key } from '@/store';
 import screenfull from 'screenfull';
 import HeadImg from '@/assets/head02.gif';
-import { ElMessage } from 'element-plus';
-
+import { Tabs, Badge, Breadcrumb, message, Avatar, Dropdown, Menu } from 'ant-design-vue';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  FullscreenOutlined,
+  FullscreenExitOutlined,
+  BellOutlined,
+  SkinOutlined,
+  SettingOutlined,
+  PoweroffOutlined,
+  UserOutlined
+} from '@ant-design/icons-vue';
 import style from './index.module.scss';
 
 export default defineComponent({
@@ -12,19 +22,16 @@ export default defineComponent({
     const store = useStore(key);
 
     const data = reactive({
-      menuTouchClass: 'el-icon-s-fold',
       // 全屏状态
-      isFullscreen: false
+      isFullscreen: false,
+      // 通知内容显示
+      bellContent: false
     });
+
     const breadcrumbs = computed(() => store.state.setting.breadcrumbs);
 
     // 隐藏显示菜单图标样式，watch写法
-    watch(
-      () => store.state.setting.aside === 'open',
-      (state) => {
-        data.menuTouchClass = state ? 'el-icon-s-fold' : 'el-icon-s-unfold';
-      }
-    );
+    const menuSwitch = computed(() => store.state.setting.aside);
 
     const adjustMenu = () => {
       store.commit('setting/asideState', {
@@ -38,11 +45,7 @@ export default defineComponent({
           screenfull.exit();
         } else screenfull.request();
       } else {
-        ElMessage({
-          showClose: true,
-          message: '浏览器不支持全屏',
-          type: 'error'
-        });
+        message.error('浏览器不支持全屏');
       }
     };
 
@@ -52,82 +55,83 @@ export default defineComponent({
       });
     }
 
-    // return {
-    //   data,
-    //   breadcrumbs,
-    //   HeadImg,
-    //   adjustMenu,
-    //   fullScreen
-    // };
-
     return () => (
       <div class={style['layout-bar']}>
         <ul class={style['layout-bar-left']}>
           <li onClick={adjustMenu} class={style['cper']}>
-            <i class={data.menuTouchClass} />
+            {menuSwitch.value === 'open' ? <MenuFoldOutlined /> : <MenuUnfoldOutlined />}
           </li>
           <li class={style['breadcrumb-help']}>
-            <el-breadcrumb separator="/">
+            <Breadcrumb separator="/">
               {breadcrumbs.value.map((breadcrumb, index) => (
-                <el-breadcrumb-item key={breadcrumb + index}>
-                  {breadcrumb}
-                </el-breadcrumb-item>
+                <Breadcrumb.Item key={breadcrumb + index}>{breadcrumb}</Breadcrumb.Item>
               ))}
-            </el-breadcrumb>
+            </Breadcrumb>
           </li>
         </ul>
         <ul class={style['layout-bar-right']}>
-          <el-popover
-            placement="top-end"
-            trigger="click"
-            v-slots={{
-              reference: (
-                <li>
-                  <el-badge value={12} class={style['item']}>
-                    <i class="el-icon-bell" />
-                  </el-badge>
-                </li>
-              )
-            }}
-          >
-            <el-tabs value="first">
-              <el-tab-pane label="通知" name="first">
-                通知
-              </el-tab-pane>
-              <el-tab-pane label="消息" name="second">
-                消息
-              </el-tab-pane>
-              <el-tab-pane label="邮件" name="third">
-                邮件
-              </el-tab-pane>
-            </el-tabs>
-          </el-popover>
-          <li>
-            <i class="el-icon-table-lamp" />
-          </li>
-          <li onClick={fullScreen}>
-            <i class={data.isFullscreen ? 'el-icon-aim' : 'el-icon-full-screen'} />
-          </li>
-          <li>
-            <i class="el-icon-setting" />
-          </li>
-          <el-dropdown
-            v-slots={{
-              dropdown: (
-                <el-dropdown-menu>
-                  <el-dropdown-item icon="el-icon-user">个人中心</el-dropdown-item>
-                  <el-dropdown-item icon="el-icon-setting">个人设置</el-dropdown-item>
-                  <el-dropdown-item icon="el-icon-switch-button" divided>
-                    退出登录
-                  </el-dropdown-item>
-                </el-dropdown-menu>
-              )
-            }}
+          <Dropdown
+            overlayClassName={style.notice}
+            trigger={['click']}
+            placement="bottomCenter"
+            overlay={
+              <>
+                <div class={style['notice-content']}>
+                  <Tabs defaultActiveKey={1}>
+                    <Tabs.TabPane key={1} tab="通知">
+                      通知内容
+                    </Tabs.TabPane>
+                    <Tabs.TabPane key={2} tab="消息">
+                      消息内容
+                    </Tabs.TabPane>
+                    <Tabs.TabPane key={3} tab="邮件">
+                      邮件内容
+                    </Tabs.TabPane>
+                  </Tabs>
+                </div>
+              </>
+            }
           >
             <li>
-              <el-avatar size="small" src={HeadImg} />
+              <Badge dot class={style['item']}>
+                <BellOutlined />
+              </Badge>
             </li>
-          </el-dropdown>
+          </Dropdown>
+          <li>
+            <SkinOutlined />
+          </li>
+          <li onClick={fullScreen}>
+            {data.isFullscreen ? <FullscreenOutlined /> : <FullscreenExitOutlined />}
+          </li>
+          <li>
+            <SettingOutlined />
+          </li>
+          <Dropdown
+            trigger={['click']}
+            overlay={
+              <Menu>
+                <Menu.Item>
+                  <UserOutlined /> 个人中心
+                </Menu.Item>
+                <Menu.Item>
+                  <SettingOutlined />
+                  个人设置
+                </Menu.Item>
+                <Menu.Item>
+                  <PoweroffOutlined />
+                  退出登录
+                </Menu.Item>
+              </Menu>
+            }
+            // v-slots={{
+            //   overlay:
+            // }}
+          >
+            <li>
+              <Avatar size="small" src={HeadImg} />
+            </li>
+          </Dropdown>
         </ul>
       </div>
     );
