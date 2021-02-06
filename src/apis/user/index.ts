a8819d22958a56e2e7fb779fd678cea57a3467f9
zhoubangfu <zbfcqtl@163.com>
2021-02-06 17:16:52 +0800

    feat: 登录相关接口调用方法

--- src/apis/user/index.ts
+++ src/apis/user/index.ts
@@ -1,1 +1,13 @@
 import axios from '@/utils/request';
+
+interface LoginInfoType {
+  username: string;
+  password: string;
+}
+
+// 登出
+export const logout = (): Promise<any> => axios.post('logout');
+// 登录
+export const login = (data: LoginInfoType): Promise<any> => axios.post('login', data);
+// 获取信息
+export const getUseInfo = (): Promise<any> => axios.get('info');
