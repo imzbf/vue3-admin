5740bc8cb903090810aee2be4e73e417f47d293b
zhoubangfu <zbfcqtl@163.com>
2021-04-08 15:01:30 +0800

    feat: 首页布局,vue模板组件demo

---
+++ src/views/vueDemo.vue
@@ -0,0 +1,24 @@
+<template>
+  <fe-card>
+    <template #title>
+      <span>这里标题</span>
+    </template>
+    <template #func>
+      <info-circle-outlined />
+    </template>
+    <span>这里是使用的vue模板组件！</span>
+  </fe-card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { InfoCircleOutlined } from '@ant-design/icons-vue';
+import FeCard from '@/components/Card';
+
+export default defineComponent({
+  components: {
+    'fe-card': FeCard,
+    'info-circle-outlined': InfoCircleOutlined
+  }
+});
+</script>
