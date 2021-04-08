3916602c6bc929ecde142a9ff7f5753337d3746b
zhoubangfu <zbfcqtl@163.com>
2021-04-08 15:00:38 +0800

    feat: 自定义card组件(未优化)

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,27 @@
+import { defineComponent, SetupContext, EmitsOptions } from 'vue';
+import { getSlot } from '@/utils/vue-tsx';
+import './index.scss';
+
+const Card = defineComponent({
+  props: {
+    title: null
+  },
+  setup(props, ctx: SetupContext<EmitsOptions>) {
+    console.log(props, ctx);
+    const slotDefault = getSlot({ ctx });
+    const slotTitle = getSlot({ props, ctx }, 'title');
+    const slotFunc = getSlot({ props, ctx }, 'func');
+
+    return () => (
+      <div class="fe-card">
+        <div class="fe-card-header">
+          <div class="fe-card-title">{slotTitle}</div>
+          <div class="fe-card-func">{slotFunc}</div>
+        </div>
+        <div class="fe-card-body">{slotDefault}</div>
+      </div>
+    );
+  }
+});
+
+export default Card;
