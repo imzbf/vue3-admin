75b7630de0acb8d8bb6aba1f52360cb7acf4baf0
zhoubangfu <zbfcqtl@163.com>
2021-04-14 18:07:16 +0800

    refactor: 修改新的图标地址

--- src/config/urls.ts
+++ src/config/urls.ts
@@ -1,6 +1,6 @@
 // 头像地址
 const DEMO_USER_HEAD = '/static/img/head02.gif';
 // 矢量图标库地址
-const ICON_FONT = '//at.alicdn.com/t/font_2365632_ogl3iq2zcza.js';
+const ICON_FONT = '//at.alicdn.com/t/font_2365632_5rlyozbncwj.js';
 
 export { DEMO_USER_HEAD, ICON_FONT };
