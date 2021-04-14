2d60f584be499d139d437508e1bf3004dc04375c
zhoubangfu <zbfcqtl@163.com>
2021-04-14 22:57:57 +0800

    feat: 公共的iconfont组件

---
+++ src/components/IconFont/index.tsx
@@ -0,0 +1,8 @@
+import { createFromIconfontCN } from '@ant-design/icons-vue';
+import { ICON_FONT } from '@/config/urls';
+
+const MyIcon = createFromIconfontCN({
+  scriptUrl: ICON_FONT
+});
+
+export default MyIcon;
