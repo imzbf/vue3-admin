80e427ff6a4f8bfcb9d787ea473b843594a5f0b8
zhoubangfu <zbfcqtl@163.com>
2021-04-14 17:57:16 +0800

    feat: 添加矢量图标库地址

--- src/config/urls.ts
+++ src/config/urls.ts
@@ -1,3 +1,6 @@
-export default {
-  DEMO_USER_HEAD: '/static/img/head02.gif'
-};
+// 头像地址
+const DEMO_USER_HEAD = '/static/img/head02.gif';
+// 矢量图标库地址
+const ICON_FONT = '//at.alicdn.com/t/font_2365632_ogl3iq2zcza.js';
+
+export { DEMO_USER_HEAD, ICON_FONT };
