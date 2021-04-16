2edfee67551aca3c399768d534fe667789be6046
zhoubangfu <zbfcqtl@163.com>
2021-04-16 12:31:15 +0800

    fix: 插槽放置render函数中

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -8,19 +8,22 @@ const Card = defineComponent({
     func: null
   },
   setup(props, ctx: SetupContext<EmitsOptions>) {
-    const slotDefault = getSlot({ ctx });
-    const slotTitle = getSlot({ props, ctx }, 'title');
-    const slotFunc = getSlot({ props, ctx }, 'func');
+    return () => {
+      // 插槽一定当然函数组件（render）方法内部，否则提示：this will not track dependencies used in the slot
+      const slotDefault = getSlot({ ctx });
+      const slotTitle = getSlot({ props, ctx }, 'title');
+      const slotFunc = getSlot({ props, ctx }, 'func');
 
-    return () => (
-      <div class="fe-card">
-        <div class="fe-card-header">
-          <div class="fe-card-title">{slotTitle}</div>
-          <div class="fe-card-func">{slotFunc}</div>
+      return (
+        <div class="fe-card">
+          <div class="fe-card-header">
+            <div class="fe-card-title">{slotTitle}</div>
+            <div class="fe-card-func">{slotFunc}</div>
+          </div>
+          <div class="fe-card-body">{slotDefault}</div>
         </div>
-        <div class="fe-card-body">{slotDefault}</div>
-      </div>
-    );
+      );
+    };
   }
 });
 
