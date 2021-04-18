e8f7ab3d132baf8bd57b6674ce80b5a3ce93d9fc
zhoubangfu <zbfcqtl@163.com>
2021-04-18 11:58:36 +0800

    refactor: 添加外部头/内容样式prop

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,11 +1,41 @@
-import { defineComponent, SetupContext, EmitsOptions } from 'vue';
+import {
+  defineComponent,
+  SetupContext,
+  EmitsOptions,
+  PropType,
+  CSSProperties
+} from 'vue';
 import { getSlot } from '@/utils/vue-tsx';
 import './index.scss';
 
+import classnames from 'classnames';
+
 const Card = defineComponent({
   props: {
-    title: null,
-    func: null
+    title: {
+      type: [Object, String] as PropType<JSX.Element | string>,
+      default: undefined
+    },
+    func: {
+      type: [Object, String] as PropType<JSX.Element | string>,
+      default: undefined
+    },
+    headerClass: {
+      type: String,
+      default: ''
+    },
+    headerStyle: {
+      type: [Object, String] as PropType<CSSProperties | string>,
+      default: ''
+    },
+    bodyClass: {
+      type: String,
+      default: ''
+    },
+    bodyStyle: {
+      type: [Object, String] as PropType<CSSProperties | string>,
+      default: ''
+    }
   },
   setup(props, ctx: SetupContext<EmitsOptions>) {
     return () => {
@@ -14,13 +44,18 @@ const Card = defineComponent({
       const slotTitle = getSlot({ props, ctx }, 'title');
       const slotFunc = getSlot({ props, ctx }, 'func');
 
+      const headerClass = classnames('fe-card-header', props.headerClass);
+      const bodyClass = classnames('fe-card-body', props.bodyClass);
+
       return (
         <div class="fe-card">
-          <div class="fe-card-header">
+          <div class={headerClass} style={props.headerStyle}>
             <div class="fe-card-title">{slotTitle}</div>
             <div class="fe-card-func">{slotFunc}</div>
           </div>
-          <div class="fe-card-body">{slotDefault}</div>
+          <div class={bodyClass} style={props.bodyStyle}>
+            {slotDefault}
+          </div>
         </div>
       );
     };
