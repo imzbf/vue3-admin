7df0725c2d6e057f17901839e9dd22078f3439f8
zhoubangfu <zbfcqtl@163.com>
2021-04-18 12:01:58 +0800

    feat: 工作台记录模块

---
+++ src/views/Home/workspace/Body/Record.tsx
@@ -0,0 +1,124 @@
+import { defineComponent, reactive } from 'vue';
+import FeCard from '@/components/Card';
+import { List, Avatar } from 'ant-design-vue';
+import IconFont from '@/components/IconFont';
+import styles from '../index.module.scss';
+
+interface DataItem {
+  title: JSX.Element;
+  time: string;
+  avatar: string;
+}
+const data: DataItem[] = [
+  {
+    title: (
+      <>
+        <b>
+          <a href="#">毛利小五郎</a>
+        </b>
+        &nbsp;创建了文章&nbsp;
+        <b>
+          <a href="#">前端学习笔记</a>
+        </b>
+      </>
+    ),
+    time: '8小时前',
+    avatar: '/static/img/boy01.jpg'
+  },
+  {
+    title: (
+      <>
+        <b>
+          <a href="#">江户川柯南</a>
+        </b>
+        &nbsp;创建了文章&nbsp;
+        <b>
+          <a href="#">React与Vue的状态更新对比</a>
+        </b>
+      </>
+    ),
+    time: '23小时前',
+    avatar: '/static/img/boy01.jpg'
+  },
+  {
+    title: (
+      <>
+        <b>
+          <a href="#">工藤优作</a>
+        </b>
+        &nbsp;创建了文章&nbsp;
+        <b>
+          <a href="#">简单版本的echarts</a>
+        </b>
+      </>
+    ),
+    time: '2021年04月16日18:24:51',
+    avatar: '/static/img/boy01.jpg'
+  },
+  {
+    title: (
+      <>
+        <b>
+          <a href="#">毛利兰</a>
+        </b>
+        &nbsp;创建了文章&nbsp;
+        <b>
+          <a href="#">魔幻的css（一）</a>
+        </b>
+      </>
+    ),
+    time: '2021年03月16日18:24:59',
+    avatar: '/static/img/boy01.jpg'
+  },
+  {
+    title: (
+      <>
+        <b>
+          <a href="#">赤井秀一</a>
+        </b>
+        &nbsp;创建了文章&nbsp;
+        <b>
+          <a href="#">Java从入门到猝死</a>
+        </b>
+      </>
+    ),
+    time: '2021年02月16日18:25:08',
+    avatar: '/static/img/boy01.jpg'
+  }
+];
+
+// 进行中的
+const ProjectSchedule = defineComponent({
+  setup() {
+    const listData = reactive({
+      list: data
+    });
+
+    return () => (
+      <FeCard
+        class="va-mb"
+        title={
+          <>
+            <IconFont type="icon-dongtai" class={styles['title-icon']} />
+            动态
+          </>
+        }
+      >
+        <List
+          dataSource={listData.list}
+          renderItem={({ item }: { item: DataItem }) => (
+            <List.Item>
+              <List.Item.Meta
+                title={item.title}
+                avatar={<Avatar src={item.avatar} />}
+                description={item.time}
+              ></List.Item.Meta>
+            </List.Item>
+          )}
+        ></List>
+      </FeCard>
+    );
+  }
+});
+
+export default ProjectSchedule;
