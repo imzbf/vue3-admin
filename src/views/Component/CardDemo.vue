da117350b230815c2a2be1ba9f3ad77891fef6a0
zhoubangfu <zbfcqtl@163.com>
2021-04-21 22:32:21 +0800

    feat: card组件demo

---
+++ src/views/Component/CardDemo.vue
@@ -0,0 +1,19 @@
+<template>
+  <vra-card headerStyle="font-weight: bold; font-size: 16px;">
+    <template v-slot:title> 这是使用vue模块开发的 </template>
+    <p>标题使用props.title或title插槽</p>
+    <p>内容默认插槽</p>
+    <p>样式：headerStyle="font-weight: bold; font-size: 16px;"</p>
+  </vra-card>
+</template>
+
+<script lang="ts">
+import VraCard from '@/components/Card';
+
+export default {
+  name: 'CardDemo',
+  components: {
+    'vra-card': VraCard
+  }
+};
+</script>
