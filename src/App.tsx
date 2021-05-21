492b14b5d98920a4a0167a92509286f4c7151257
zhoubangfu <zbfcqtl@163.com>
2021-05-21 17:33:46 +0800

    feat: 登录适配暗黑模式

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
 import { defineComponent } from 'vue';
 import zhCN from 'ant-design-vue/es/locale-provider/zh_CN';
 import { ConfigProvider } from 'ant-design-vue';
+import darkVars from '@/config/dark.json';
+import lightVars from '@/config/light.json';
+import { key } from '@/store';
+import { useStore } from 'vuex';
 
 export default defineComponent({
   setup() {
     const local = zhCN;
+    const store = useStore(key);
+
+    switch (store.state.setting.theme) {
+      case 'dark': {
+        (window as any).less.modifyVars(darkVars);
+        break;
+      }
+      case 'light': {
+        (window as any).less.modifyVars(lightVars);
+      }
+    }
+
     return () => (
       <ConfigProvider locale={local}>
-        <router-view />
+        <div class={`theme-wrapper theme-${store.state.setting.theme}`}>
+          <router-view />
+        </div>
       </ConfigProvider>
     );
   }
