d4228bc3ff50b03c1e98c684eb1e9f80afd283db
zbf <zbfcqtl@163.com>
2021-11-25 15:04:08 +0800

    build: mock

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,39 @@
 import path from 'path';
-import { defineConfig } from 'vite';
+import { ConfigEnv, UserConfigExport } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import ElementPlus from 'unplugin-element-plus/vite';
+import { viteMockServe } from 'vite-plugin-mock';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 6677
-  },
-  resolve: {
-    alias: {
-      // 键必须以斜线开始和结束
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "@/styles/element-var.scss" as *;`
+export default ({ command, mode }: ConfigEnv): UserConfigExport => {
+  return {
+    server: {
+      port: 6677
+    },
+    resolve: {
+      alias: {
+        // 键必须以斜线开始和结束
+        '@': path.resolve(__dirname, './src')
       }
-    }
-  },
-  plugins: [
-    vue(),
-    ElementPlus({
-      useSource: true
-    })
-  ]
-});
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/styles/element-var.scss" as *;`
+        }
+      }
+    },
+    plugins: [
+      vue(),
+      viteMockServe({
+        ignore: /^_/,
+        mockPath: 'mock',
+        watchFiles: true, // 修改更新
+        localEnabled: command === 'serve'
+      }),
+      ElementPlus({
+        useSource: true
+      })
+    ]
+  };
+};
