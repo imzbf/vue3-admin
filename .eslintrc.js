e27e376b1714f5885fc622215c33f8388435716a
zbf <zbfcqtl@163.com>
2021-11-25 15:03:27 +0800

    chore: code format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,23 @@
 module.exports = {
+  env: {
+    browser: true,
+    node: true
+  },
   extends: [
     // add more generic rulesets here, such as:
     // 'eslint:recommended',
-    'plugin:vue/vue3-recommended'
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
     // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
   ],
+  plugins: ['@typescript-eslint'],
   rules: {
-    'vue/multi-word-component-names': 'off'
+    'vue/multi-word-component-names': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off'
   }
 };
