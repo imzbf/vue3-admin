1ee6a33a6458953e25b38cade55fdbf56ae7dca8
zhoubangfu <zbfcqtl@163.com>
2021-12-05 22:19:10 +0800

    feat: 团队

--- src/pages/Dashboard/Body/index.vue
+++ src/pages/Dashboard/Body/index.vue
@@ -2,8 +2,8 @@
 import RunningProject from './RunningProject.vue';
 import CommonlyUsed from './CommonlyUsed.vue';
 // import Task from './Task';
-// import Record from './Record';
-// import Team from './Team';
+// import Record from './Record.vue';
+import Team from './Team.vue';
 </script>
 <template>
   <div class="view-space dashboard-body">
