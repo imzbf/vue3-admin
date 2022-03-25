7d4c332491e92f1ec6b8c04d56acbf507687c23c
zbf <zbfcqtl@gmail.com>
2022-03-25 14:11:55 +0800

    fix: 深层keep-alive错误

--- src/pages/Component/Base/index.vue
+++ src/pages/Component/Base/index.vue
@@ -1,16 +1,3 @@
 <template>
-  <router-view v-slot="{ Component }">
-    <transition name="fade" mode="out-in">
-      <keep-alive :include="store.state.setting.cacheList">
-        <component :is="Component" />
-      </keep-alive>
-    </transition>
-  </router-view>
+  <router-view> </router-view>
 </template>
-
-<script lang="ts" setup>
-import { useStore } from 'vuex';
-import { key } from '@/store';
-
-const store = useStore(key);
-</script>
