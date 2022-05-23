6a188359ebe49e33e1c57e10ee183bc0c069efae
imzbf <37092006+imzbf@users.noreply.github.com>
2022-05-23 09:16:43 +0000

    Deploying to preview from @ imzbf/vue3-admin@dd20a01d73c5407229ec689fa7ea774931acebd7 🚀

---
+++ docs/assets/index.ac6b9608.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./plugin-vue_export-helper.21dcd24c.js";import{a as r,o as t,c}from"./index.f1354cb2.js";const n={};function _(a,s){const e=r("router-view");return t(),c(e)}var m=o(n,[["render",_]]);export{m as default};
--- docs/assets/table.ba4a2bab.js
+++
@@ -1,1 +0,0 @@
-import{aC as e}from"./index.bcf223ae.js";const o=a=>e.get("/table",{params:a});export{o as q};
