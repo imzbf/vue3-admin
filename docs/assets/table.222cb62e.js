590b3f4527f9ee4a69f12d095c9025f1095c18b6
imzbf <37092006+imzbf@users.noreply.github.com>
2022-06-06 13:09:54 +0000

    Deploying to preview from @ imzbf/vue3-admin@7f6508f040233b5d1d701680059451eec5242cc9 🚀

--- docs/assets/table.222cb62e.js
+++
@@ -1,1 +0,0 @@
-import{aE as e}from"./index.90ff9981.js";const o=a=>e.get("/table",{params:a});export{o as q};
---
+++ docs/assets/table.b0660a51.js
@@ -0,0 +1,1 @@
+import{aE as e}from"./index.91ceb0ae.js";const o=a=>e.get("/table",{params:a});export{o as q};
