ca63df137de43f113daa72975efa72a4c0123165
zbf <zbfcqtl@gmail.com>
2022-10-17 22:54:15 +0800

    chore: update docker configuration

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# chmod +x ./build.sh
+
+DOCKER_IMAGE_NAME="vue3-admin-image"
+DOCKER_CONTAINER_NAME="vue3-admin-container"
+
+if [[ -n $(docker images -q ${DOCKER_IMAGE_NAME}) ]];then
+	echo "====delete${DOCKER_IMAGE_NAME}===="
+	docker rmi -f ${DOCKER_IMAGE_NAME}
+fi
+
+echo "====build image===="
+# -f ./docker/Dockerfile
+docker build --no-cache -t ${DOCKER_IMAGE_NAME} .
+
+if [[ -n $(docker ps -q -a -f "name=^${DOCKER_CONTAINER_NAME}$") ]];then
+	echo "====remove ${DOCKER_CONTAINER_NAME}===="
+	docker rm -f ${DOCKER_CONTAINER_NAME}
+fi
+
+echo "====run===="
+docker run -p 3033:80 -d --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
+
+echo "====delete <none>===="
+docker rmi $(docker images | grep "none" | awk '{print $3}')
+
+docker images
+docker ps
