a2175fcf2a56ad179cf4b0ff7dd09c73d79ec4b7
imzbf <zbfcqtl@gmail.com>
2023-11-04 22:32:00 +0800

    feat: upgrade dependency and fix got wrong width of component input

--- src/layouts/Header/Message.vue
+++ src/layouts/Header/Message.vue
@@ -6,44 +6,4 @@
 export default { name: 'IzMessage' };
 </script>
 
-<script setup lang="ts">
-import { reactive } from 'vue';
-
-interface DataItem {
-  avatar: string;
-  title: string;
-  desc: string;
-  date: string;
-}
-
-const VITE_BASE = import.meta.env.VITE_BASE;
-
-const data = reactive<{ list: DataItem[] }>({
-  list: [
-    {
-      avatar: `${VITE_BASE}/static/img/boy01.jpg`,
-      title: '小五郎 评论了你',
-      desc: '柯南要请饭，收到请回话！',
-      date: '两天前'
-    },
-    {
-      avatar: `${VITE_BASE}/static/img/head02.gif`,
-      title: '小五郎 评论了你',
-      desc: '柯南要请饭，收到请回话！',
-      date: '两天前'
-    },
-    {
-      avatar: `${VITE_BASE}/static/img/boy01.jpg`,
-      title: '小五郎 评论了你',
-      desc: '柯南要请饭，收到请回话！',
-      date: '两天前'
-    },
-    {
-      avatar: `${VITE_BASE}/static/img/boy01.jpg`,
-      title: '小五郎 评论了你',
-      desc: '柯南要请饭，收到请回话！',
-      date: '两天前'
-    }
-  ]
-});
-</script>
+<script setup lang="ts"></script>
